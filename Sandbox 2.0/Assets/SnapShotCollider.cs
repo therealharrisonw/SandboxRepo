30ca82fb15fd227b152d0f5f42553bcfcad0aff2
therealharrisonw <90920251+therealharrisonw@users.noreply.github.com>
2021-10-20 14:56:24 -0600

    Audio Lab Tested and Done

--- Sandbox 2.0/Assets/SnapShotCollider.cs
+++ Sandbox 2.0/Assets/SnapShotCollider.cs
@@ -9,16 +9,14 @@ public class SnapShotCollider : MonoBehaviour
     public AudioMixerSnapshot day;
     bool isNight = false;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "player")
+        if(other.gameObject.tag == "Player")
         {
+            
             if (!isNight)
             {
                 night.TransitionTo(0.5f);
