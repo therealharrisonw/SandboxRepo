100be01b69abe7938fe9138d7b99cc3df3474253
therealharrisonw <90920251+therealharrisonw@users.noreply.github.com>
2021-10-21 17:20:06 -0600

    Element Manager Comments Finished

--- Sandbox 2.0/Assets/Scripts/Locomotion and Interactions Prototype/ElementShoot.cs
+++ Sandbox 2.0/Assets/Scripts/Locomotion and Interactions Prototype/ElementShoot.cs
@@ -13,25 +13,26 @@ public class ElementShoot : MonoBehaviour
     GameObject lightningOrb;//allows me to set the gameobject to be spawned for lightning orb
     [SerializeField]
     Transform spwnpoint; //allows me to set the transform for the orbs to spawn out of the end of the gun.
-    private bool element = true; 
+    private bool element = true; //bool used for switching the ammo type
+
     // Start is called before the first frame update
     void Awake()
     {
-        GI = GetComponent<XRGrabInteractable>();
-        GI.activated.AddListener(SpawnElements);
+        GI = GetComponent<XRGrabInteractable>(); //tells the code to grab the components from the XR Grab Interactable 
+        GI.activated.AddListener(SpawnElements); //tells the script tp add a listner for when the trigger is fired
     }
 
     private void SpawnElements(ActivateEventArgs arg0)
     {
-        if (element)
+        if (element) //if element is true do this
         {
-            Instantiate(waterOrb, spwnpoint.position, Quaternion.identity);
-            element = false;
+            Instantiate(waterOrb, spwnpoint.position, Quaternion.identity); //spawns the water orb out of the end of the gun
+            element = false; //switches the ammo type to lightning. 
         }
         else
         {
-            Instantiate(lightningOrb, spwnpoint.position, Quaternion.identity);
-            element = true;
+            Instantiate(lightningOrb, spwnpoint.position, Quaternion.identity); //spawns the lightning orb out of the end of the gun
+            element = true; //switches the ammo type to water
         }
         
         
