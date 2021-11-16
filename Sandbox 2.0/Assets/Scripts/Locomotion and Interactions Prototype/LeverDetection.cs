7ec96a6a7de3ba4902dce510ae60cc8ec178b34d
therealharrisonw <90920251+therealharrisonw@users.noreply.github.com>
2021-11-16 09:17:12 -0700

    Setting Up Unity Event

---
+++ Sandbox 2.0/Assets/Scripts/Locomotion and Interactions Prototype/LeverDetection.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LeverDetection : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent levergang;
+     HingeJoint hJoint;
+
+    private void Start()
+    {
+        hJoint = GetComponent<HingeJoint>(); //teels the hinge joint to take that information
+    }
+
+    private void checkLever()
+    {
+        if (hJoint.angle == hJoint.limits.max)
+        {
+            levergang.Invoke();
+        }
+    }
+
+}
+
